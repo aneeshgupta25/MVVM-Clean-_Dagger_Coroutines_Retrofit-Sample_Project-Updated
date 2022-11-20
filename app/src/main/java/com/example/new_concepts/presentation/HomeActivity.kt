9e2f343618a080d46e4071873cc6df81c7625596
aneeshgupta25 <aneeshgupta2512@gmail.com>
2022-11-21 05:03:48 +0530

    Home Screen Layout

--- app/src/main/java/com/example/new_concepts/presentation/HomeActivity.kt
+++ app/src/main/java/com/example/new_concepts/presentation/HomeActivity.kt
@@ -1,10 +1,10 @@
-package com.example.new_concepts
+package com.example.new_concepts.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mvvm_clean__dagger_coroutines_retrofit_sample_project_updated.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

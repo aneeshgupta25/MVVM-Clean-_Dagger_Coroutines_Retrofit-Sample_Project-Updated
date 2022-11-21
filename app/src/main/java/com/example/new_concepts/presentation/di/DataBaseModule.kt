97523e425525730dc061b303ab84781fef16cacd
aneeshgupta25 <aneeshgupta2512@gmail.com>
2022-11-21 12:23:25 +0530

    Database Module (Dagger Module)

---
+++ app/src/main/java/com/example/new_concepts/presentation/di/DataBaseModule.kt
@@ -0,0 +1,41 @@
+package com.example.new_concepts.presentation.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.new_concepts.data.db.ArtistDao
+import com.example.new_concepts.data.db.MovieDao
+import com.example.new_concepts.data.db.TMDBDatabase
+import com.example.new_concepts.data.db.TvShowsDao
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class DataBaseModule {
+
+    @Singleton
+    @Provides
+    fun provideMovieDataBase(context: Context):TMDBDatabase {
+        return Room.databaseBuilder(context, TMDBDatabase::class.java, "tmdbclient")
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideMovieDao(tmdbDatabase: TMDBDatabase) : MovieDao {
+        return tmdbDatabase.movieDao()
+    }
+
+    @Singleton
+    @Provides
+    fun provideTvShowDao(tmdbDatabase: TMDBDatabase) : TvShowsDao {
+        return tmdbDatabase.tvDao()
+    }
+
+    @Singleton
+    @Provides
+    fun provideArtistDao(tmdbDatabase: TMDBDatabase) : ArtistDao {
+        return tmdbDatabase.artistDao()
+    }
+
+}
